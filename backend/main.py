cf094772bf2a3b83991df2ab00f5f94e9b111cda
Aniketgiram <giram.a@northeastern.edu>
2024-03-30 09:39:16 -0400

    fixes

--- backend/main.py
+++ backend/main.py
@@ -20,11 +20,11 @@ S3_BUCKET_NAME = 'bigdata-assignment-04'
 s3_client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
 @app.post("/upload")
-async def upload_file(file: UploadFile = File(...)):
+async def upload_file(files: UploadFile = File(...)):
     try:
-        s3_client.upload_fileobj(file.file, S3_BUCKET_NAME, file.filename)
+        s3_client.upload_fileobj(files.file, S3_BUCKET_NAME, files.filename)
         print("file uploaded successfully")
-        triggerAirFlowPipeline(f'https://{S3_BUCKET_NAME}.s3.amazonaws.com/{file.filename}')
+        triggerAirFlowPipeline(f'https://{S3_BUCKET_NAME}.s3.amazonaws.com/{files.filename}')
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
     return {"message": "File(s) uploaded successfully"}
--- frontend/main.py
+++ frontend/main.py
@@ -11,20 +11,18 @@ if file:
     # Button to trigger file upload
     if st.button("Upload to S3"):
         # Send file to FastAPI endpoint
-        files = {"files": (file.name, file.getvalue())}
+        files = {"file": (file.name, file.getvalue())}
         response = requests.post("http://backend:8000/upload", files=files, stream=True)
-
-        # Get total file size
-        total_size = int(response.headers.get('content-length', 0))
-
-        # Upload file in chunks and update progress bar
-        uploaded_size = 0
-        for chunk in response.iter_content(chunk_size=1024):
-            uploaded_size += len(chunk)
-            progress = uploaded_size / total_size
-            progress_bar.progress(progress)
 
         if response.status_code == 200:
             st.success("File uploaded successfully to S3!")
         else:
             st.error("Failed to upload file. Error: {}".format(response.text))
+
+        # total_size = int(response.headers.get('content-length', 0))
+
+        # uploaded_size = 0
+        # for chunk in response.iter_content(chunk_size=1024):
+        #     uploaded_size += len(chunk)
+        #     progress = uploaded_size / total_size
+        #     progress_bar.progress(progress)
